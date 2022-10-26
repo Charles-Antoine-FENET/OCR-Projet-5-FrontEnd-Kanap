95208930c98dc0b395408bf2a3b71bd4016af820
Charles-Antoine FENET <108330821+Charles-Antoine-FENET@users.noreply.github.com>
2022-10-26 10:53:17 +0200

    Refacto my fetch for index.html and product.html

--- front/js/product.js
+++ front/js/product.js
@@ -17,42 +17,148 @@ fetch("http://localhost:3000/api/products/" + currentId)
       return apiProductsResult.json();
     }
   })
-  .then(function (dataProduct) {
-    function creatProduct() {
-      // Initialisation des éléments à creer dans le DOM
-      let newImg = document.createElement("img");
-      newImg.setAttribute("src", dataProduct.imageUrl);
-      newImg.setAttribute("alt", dataProduct.altTxt);
-      console.log(newImg);
+  .then((jsonData) => displayProductPage(jsonData));
 
-      //   Positionnement sur l'élément qui va recevoir les nouveaux éléments
-      let getItemImg = document.querySelector(".item__img");
 
-      //   Ajoût l'image à la div "item__img"
-      getItemImg.appendChild(newImg);
+  // Création de la fonction qui va creer les éléments nécessaire à la page
+  function displayProductPage (dataProduct){
+    let getItemImg = document.querySelector(".item__img");
+    getItemImg.innerHTML=`<img src=${dataProduct.imageUrl} alt=${dataProduct.altTxt}>`
+    document.getElementById("title").textContent = dataProduct.name;
+    document.getElementById("price").textContent = dataProduct.price;
+    document.getElementById("description").textContent = dataProduct.description;
 
-      // Renseigner les balises avec les data du fetch
-      document.getElementById("title").textContent = dataProduct.name;
-      document.getElementById("price").textContent = dataProduct.price;
-      document.getElementById("description").textContent = dataProduct.description;
+    // Creation des choix multiples
+    for (colorChoice of dataProduct.colors) {
+      console.log(dataProduct.colors[colorChoice]);
+    // Initialisation des éléments à creer dans le DOM
+      function creatColorChoices() {
+        let colorOption = document.createElement("option"); // Création du choix de couleur
+        document.getElementById("colors").appendChild(colorOption); //positionnement sur l'élément parent et intégration des éléments enfants
+        colorOption.setAttribute = ("value", colorChoice); // Ajoût des attributs "value" a chaque iteration de la boucle et callback sur la couleur de l'iteration.
+        colorOption.innerText = colorChoice; // Intégration de la couleur que l'itération aura donnée, dans la balise html
+      }
+      creatColorChoices(dataProduct.colors[colorChoice]); //execution de la creation des elements dans ma boucle
+  }};
 
-    //   Creation des choix multiples
-      for(colorChoice of dataProduct.colors){
-        console.log(dataProduct.colors[colorChoice])
-        // Initialisation des éléments à creer dans le DOM
-        function creatColorChoices(){
-            let colorOption = document.createElement("option"); // Création du choix de couleur
-            document.getElementById("colors").appendChild(colorOption);//positionnement sur l'élément parent et intégration des éléments enfants
-            colorOption.setAttribute = ("value", colorChoice); // Ajoût des attributs "value" a chaque iteration de la boucle et callback sur la couleur de l'iteration.
-            colorOption.innerText = colorChoice; // Intégration de la couleur que l'itération aura donnée, dans la balise html
-        }
-        creatColorChoices(dataProduct.colors[colorChoice]); //execution de la creation des elements dans ma boucle
-        
 
-        // Positionnement sur l'élément qui va recevoir les nouveaux éléments
 
-        // ajoût du choix possible2
+
+
+
+
+
+// Création de la class panier
+class Basket {
+  constructor() {
+    //ici on va creer la génération du panier qui ensuite s'effectuera a chaque instance qui sera crée.
+    let basket = localStorage.getItem("basket"); //On recupérer l'item qui contient la clé que nous avons crée dans la fonction saveBasket
+    if (basket == null) {
+      this.basket = []; //on enregistre le resultat dans une propriété du basket
+    } else {
+      this.basket = JSON.parse(basket);
+    }
+  }
+  save() {
+    localStorage.setItem("basket", JSON.stringify(this.basket)); //Cela permet d'enregistrer dans le localStorage un ensemble clé valeur. Ici on a pour clé dans le locastorage "basket" qui contiendra les valeur de la variable basket (qui est le panier que nous souhaitons enregistrer)
+    // Une fois que c'est enregister, nous allons le retrouver dans le local storage
+  }
+  add(currentId) {
+    let foundProduct = this.basket.find((productId) => productId.id == currentId._id);
+    if (foundProduct != undefined) {
+      foundProduct.quantity++;
+    } else {
+      product.quantity = 1;
+      this.basket.push(product);
+    }
+    this.save();
+  }
+  remove(products) {
+    this.basket = this.basket.filter((p) => p.id != products.id);
+    this.save();
+  }
+  changeQuantity(products, quantity) {
+    let foundProduct = this.basket.find((p) => p.id == products.id);
+    if (foundProduct != undefined) {
+      foundProduct.quantity += quantity;
+      if (foundProduct.quantity <= 0) {
+        remove(foundProduct);
+      } else {
+        this.save();
       }
     }
-    creatProduct(dataProduct);
-  });
+  }
+  getNumberProduct() {
+    let number = 0;
+    for (let product of this.basket) {
+      number += product.quantity;
+    }
+    return number;
+  }
+
+  getTotalPrice() {
+    let total = 0;
+    for (let product of this.basket) {
+      total += product.quantity * product.price;
+    }
+    return total;
+  }
+}
+
+let basketKanap = new Basket;
+console.log(basketKanap);
+basketKanap.add({"_id":"107fb5b75607497b96722bda5b504926"});
+
+
+
+
+
+
+
+// Zone d'archive des test que j'ai pu faire et qui fonctionnent mais que je n'ai pas conservé.
+
+// fetch("http://localhost:3000/api/products/" + currentId)
+//   .then(function (apiProductsResult) {
+//     if (apiProductsResult.ok) {
+//       return apiProductsResult.json();
+//     }
+//   })
+//   .then(function (dataProduct) {
+//     function creatProduct() {
+//       // Initialisation des éléments à creer dans le DOM
+//       let newImg = document.createElement("img");
+//       newImg.setAttribute("src", dataProduct.imageUrl);
+//       newImg.setAttribute("alt", dataProduct.altTxt);
+//       console.log(newImg);
+
+//       //   Positionnement sur l'élément qui va recevoir les nouveaux éléments
+//       let getItemImg = document.querySelector(".item__img");
+
+//       //   Ajoût l'image à la div "item__img"
+//       getItemImg.appendChild(newImg);
+
+//       // Renseigner les balises avec les data du fetch
+//       document.getElementById("title").textContent = dataProduct.name;
+//       document.getElementById("price").textContent = dataProduct.price;
+//       document.getElementById("description").textContent =
+//         dataProduct.description;
+
+//       //   Creation des choix multiples
+//       for (colorChoice of dataProduct.colors) {
+//         console.log(dataProduct.colors[colorChoice]);
+//         // Initialisation des éléments à creer dans le DOM
+//         function creatColorChoices() {
+//           let colorOption = document.createElement("option"); // Création du choix de couleur
+//           document.getElementById("colors").appendChild(colorOption); //positionnement sur l'élément parent et intégration des éléments enfants
+//           colorOption.setAttribute = ("value", colorChoice); // Ajoût des attributs "value" a chaque iteration de la boucle et callback sur la couleur de l'iteration.
+//           colorOption.innerText = colorChoice; // Intégration de la couleur que l'itération aura donnée, dans la balise html
+//         }
+//         creatColorChoices(dataProduct.colors[colorChoice]); //execution de la creation des elements dans ma boucle
+
+//         // Positionnement sur l'élément qui va recevoir les nouveaux éléments
+
+//         // ajoût du choix possible2
+//       }
+//     }
+//     creatProduct(dataProduct);
+//   });
--- front/js/script.js
+++ front/js/script.js
@@ -1,90 +1,95 @@
 console.log("script.js connecté !!");
 
-// Récuperrer les données de l'API
-
+// Appel à l'API via la fonction fetch
 fetch("http://localhost:3000/api/products")
-  .then(function (apiProductsResult) {
-    if (apiProductsResult.ok) {
-      return apiProductsResult.json();
-    }
-  })
-  //   Récuperation de la réponse émise par l'API dans la fonction que je nomme "dataProducts"
-  //   Puis creation d'une boucle qui va lire chaque instance de l'objet "produit" qui est disponible sur le serveur
-  //   dataProduct récupère et contient donc les résultats de l'api. getProduct est créé comme itération de la boucle for et va lire dans dans dataProduct ligne par ligne
-  .then(function (dataProducts) {
-    for (let getProduct in dataProducts) {
-      function creatProduct() {
-        // Initialisation des éléments à creer dans le DOM
-        let newLink = document.createElement("a");
-        let newArticle = document.createElement("article");
-        let newImg = document.createElement("img");
-        let newProductName = document.createElement("h3");
-        let newProductDescription = document.createElement("p");
 
-        //   Positionnement sur l'élément qui va recevoir les nouveaux éléments
-        let getSectionItems = document.getElementById("items");
+// On recupère la promesse dans un fichier json avec la fonction asynchrone .then
+.then(function (apiProductsResult) {
+  if (apiProductsResult.ok) {
+    return apiProductsResult.json();
+  }
+})
+// On donne le nom "jsonData" aux données que la promesse nous retourne. 
+// Puis on appel la fonction display en lui donnant en argument
+// les données que l'api nous a retourné.
+.then((jsonData) => display(jsonData)); 
 
-        //   Ajoût du lien dans l'élément
-        getSectionItems.appendChild(newLink);
-        newLink.setAttribute("href", `./product.html?id=${dataProducts[getProduct]._id}`);
+// Création d'une fonction "display" qui a pour but d'être invoquée sur la réponse de l'API
+function display(dataProducts) {
+  // Positionnement sur l'élément parent à ceux que nous devons créer.
+  let getSectionItems = document.getElementById("items");
+  
+  // Création d'une boucle qui va parcourir "jsonData" et creer des éléments à chaque itération.
+  for (product of dataProducts) {
+    // Initialisation des éléments que nous devons créer
+    let newLink = document.createElement("a");
+    let newArticle = document.createElement("article");
+    
+    // Création du contenu et des attributs nécessaires pour les éléments
+    newLink.setAttribute("href", `./product.html?id=${product._id}`);
+    newArticle.innerHTML =
+    `<img src=${product.imageUrl} alt=${product.altTxt}><h3 class='productName'>${product.name}</h3><p class='productDescription'>${product.description}</p>`;
+    
+    
+    // Ajoût des éléments dans le DOM
+    newLink.appendChild(newArticle);
+    getSectionItems.appendChild(newLink);
+  }
+}
 
-        //   Ajoût de l'article dans le lien
-        newLink.appendChild(newArticle);
 
-        //   Ajoût de l'img dans l'article
-        newArticle.appendChild(newImg);
-        newImg.setAttribute("src", dataProducts[getProduct].imageUrl);
-        newImg.setAttribute("alt", dataProducts[getProduct].altTxt);
 
-        //   Ajoût de titre du nouveau produit dans l'article
-        newArticle.appendChild(newProductName);
-        newProductName.classList.add("productName");
-        newProductName.innerHTML = dataProducts[getProduct].name;
-
-        //   Ajoût de la description du nouveau produit dans l'article
-        newArticle.appendChild(newProductDescription);
-        newProductDescription.classList.add("productDescription");
-        newProductDescription.innerHTML = dataProducts[getProduct].description;
-      }
-      creatProduct(dataProducts[getProduct]);
-    }
-  })
-  .catch(function (err) {
-    // Une erreur est survenue
-  });
-
-// function creatNewProduct() {
-//   // Initialisation des éléments à creer dans le DOM
-//   let newLink = document.createElement("a");
-//   let newArticle = document.createElement("article");
-//   let newImg = document.createElement("img");
-//   let newProductName = document.createElement("h3");
-//   let newProductDescription = document.createElement("p");
-//   //   Ajoût des nouveaux éléments à la balise id "items"
-//   let getSectionItems = document.getElementById("items");
-//   getSectionItems.appendChild(newLink);
-//   console.log("Lien ajouté");
-//   newLink.appendChild(newArticle);
-//   console.log("Article ajouté en enfant du lien");
-//   //   Modification du contenu de l'article avec la technique de string interpolation
-//   newArticle.innerHTML = `<img=${getProduct.imageURL} alt =${getProduct.altTxt}> <h3 class"productName">${getProduct.name}</h3> <p class="productDescription">${getProduct.description}</p>`;
-// }
-
-// creatNewProduct();
-
-// Créer un nouveau élément a chaque itération (creatElement)
+// Zone d'ariche des méthodes que j'ai pu tester mais que je n'ai pas retenu.
 
 // fetch("http://localhost:3000/api/products")
-//   .then(function (res) {
-//     if (res.ok) {
-//       return res.json();
+//   .then(function (apiProductsResult) {
+//     if (apiProductsResult.ok) {
+//       return apiProductsResult.json();
 //     }
 //   })
-//   //   Récuperation de la réponse émise par l'API
-//   .then(function (productList) {
-//     console.log(productList);
-//     console.log("Affichage de la requête validé!!!")
+//   //   Récuperation de la réponse émise par l'API dans la fonction que je nomme "dataProducts"
+//   //   Puis creation d'une boucle qui va lire chaque instance de l'objet "produit" qui est disponible sur le serveur
+//   //   dataProduct récupère et contient donc les résultats de l'api. getProduct est créé comme itération de la boucle for et va lire dans dans dataProduct ligne par ligne
+//   .then(function (dataProducts) {
+//     for (let getProduct in dataProducts) {
+//       function creatProduct() {
+//         // Initialisation des éléments à creer dans le DOM
+//         let newLink = document.createElement("a");
+//         let newArticle = document.createElement("article");
+//         let newImg = document.createElement("img");
+//         let newProductName = document.createElement("h3");
+//         let newProductDescription = document.createElement("p");
+
+//         //   Positionnement sur l'élément qui va recevoir les nouveaux éléments
+//         let getSectionItems = document.getElementById("items");
+
+//         //   Ajoût du lien dans l'élément
+//         getSectionItems.appendChild(newLink);
+//         newLink.setAttribute("href", `./product.html?id=${dataProducts[getProduct]._id}`);
+
+//         //   Ajoût de l'article dans le lien
+//         newLink.appendChild(newArticle);
+
+//         //   Ajoût de l'img dans l'article
+//         newArticle.appendChild(newImg);
+//         newImg.setAttribute("src", dataProducts[getProduct].imageUrl);
+//         newImg.setAttribute("alt", dataProducts[getProduct].altTxt);
+
+//         //   Ajoût de titre du nouveau produit dans l'article
+//         newArticle.appendChild(newProductName);
+//         newProductName.classList.add("productName");
+//         newProductName.innerHTML = dataProducts[getProduct].name;
+
+//         //   Ajoût de la description du nouveau produit dans l'article
+//         newArticle.appendChild(newProductDescription);
+//         newProductDescription.classList.add("productDescription");
+//         newProductDescription.innerHTML = dataProducts[getProduct].description;
+//       }
+//       creatProduct(dataProducts[getProduct]);
+//     }
 //   })
 //   .catch(function (err) {
 //     // Une erreur est survenue
 //   });
+
+  // On recupère via un second .then les données de la réponse de la promesse de l'API. 
