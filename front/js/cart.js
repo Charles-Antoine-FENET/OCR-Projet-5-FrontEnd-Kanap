9a095177413fb56d87e638d087c6350763e0a82b
Charles-Antoine FENET <108330821+Charles-Antoine-FENET@users.noreply.github.com>
2022-11-10 13:55:53 +0100

    Fix total price calculator by putting reduce function after my loop

--- front/js/cart.js
+++ front/js/cart.js
@@ -10,11 +10,8 @@ creatBasketProductList(basket);
 // Cette fonction doit être asynchrone car nous souhaitons attendre la réponse du serveur avant de lancer la création des éléments dans le Dom
 // Sans l'async et l'await, les éléments crées risquent de se lancer avant la réponse du serveur et donc creer une erreur d'affichage.
 async function creatBasketProductList() {
-
-
   // Création du tableau qui va contenir le prix total de chaque ligne de produits
   const total = [];
-
 
   // Création de l'itération "productOrdered" qui va parcourir le tableau "basket" qui est notre panier dans le local storage
   for (productOrdered in basket) {
@@ -32,17 +29,11 @@ async function creatBasketProductList() {
     let newArticle = document.createElement("article");
 
     // Stockage du prix total du produit commandé
-    const totalPriceArticle = basket[productOrdered].quantityOfProduct * apiDataProducts.price;
+    const totalPriceArticle =
+      basket[productOrdered].quantityOfProduct * apiDataProducts.price;
 
-    // Push du prix total du produit dans le tableau de stockage des prix totaux que nous avons créé 
+    // Push du prix total du produit dans le tableau de stockage des prix totaux que nous avons créé
     total.push(totalPriceArticle);
-
-    // Utilisation de la fonction reduce pour calculer le total de la commande.
-    const sum = (accumulateur, prices) => accumulateur + prices;
-    const totalAll = total.reduce(sum);
-
-    // intégration du resultat total des prix dans la balise HTML "totalPrice"
-    document.getElementById("totalPrice").innerHTML += `${totalAll}`;
 
     // Création du reste des éléments HTML et implémentation des données de l'api et des données issues du localstorage et de notre boucle for
     newArticle.innerHTML = `
@@ -61,10 +52,18 @@ async function creatBasketProductList() {
             <div class="cart__item__content__settings">
                 <div class="cart__item__content__settings__quantity">
                     <p>Qté :</p>
-                    <input type="number" id="${basket[productOrdered].colorOfProduct + basket[productOrdered].idOfProduct}" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${basket[productOrdered].quantityOfProduct}">
+                    <input type="number" id="${
+                      basket[productOrdered].colorOfProduct +
+                      basket[productOrdered].idOfProduct
+                    }" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${
+      basket[productOrdered].quantityOfProduct
+    }">
                 </div>
             <div class="cart__item__content__settings__delete">
-                <p class="deleteItem" id="${basket[productOrdered].idOfProduct + basket[productOrdered].colorOfProduct}">Supprimer</p>
+                <p class="deleteItem" id="${
+                  basket[productOrdered].idOfProduct +
+                  basket[productOrdered].colorOfProduct
+                }">Supprimer</p>
             </div>
         </div>
       </div>
@@ -72,12 +71,16 @@ async function creatBasketProductList() {
     `;
     document.getElementById("cart__items").appendChild(newArticle);
   }
+  // Utilisation de la fonction reduce pour calculer le total de la commande.
+  const sum = (accumulateur, prices) => accumulateur + prices;
+  const totalAll = total.reduce(sum);
+  console.log("voici le test de " + totalAll);
+
+  // intégration du resultat total des prix dans la balise HTML "totalPrice"
+  document.getElementById("totalPrice").innerHTML += `${totalAll}`;
   changedQty();
   deleteProduct();
 }
-
-
-
 
 // Création d'un fonction qui permet de modifier les quantités
 // target va chercher la cible de la fonction
@@ -92,9 +95,12 @@ function changedQty() {
       if (e.target.value <= 100 && e.target.value > 0) {
         // Si les valeurs sont bonnes alors on crée une variable "newQuantity" qui va chercher dans le panier version objet du local storage l'élément qui correspond au code id que nous avons crée (color suivi de l'id) et lui assigner la valeur de la target contenu dans changequantity
         // Pour cela un id a été ajouté dans le template au niveau de l'input. Cette id permet de créer un code spécial qui va concaténer la couleur+l'id du produit
-        const newQuantity = basket.find(product => product.colorOfProduct + product.idOfProduct === changeQuantity.id);
+        const newQuantity = basket.find(
+          (product) =>
+            product.colorOfProduct + product.idOfProduct === changeQuantity.id
+        );
         // on ajoute la nouvelle quantité contenue dans la function qui target(e) à quantityOfProduct
-        newQuantity.quantityOfProduct =+ e.target.value;
+        newQuantity.quantityOfProduct = +e.target.value;
         // on pousse les nouvelles données dans le local storage
         localStorage.setItem("basket", JSON.stringify(basket));
         // On demande à la page de se recharger pour prendre en compte à l'écran les dernières données.
@@ -106,7 +112,6 @@ function changedQty() {
   }
 }
 
-
 // Création d'une fonction qui permet de supprimer un élément du panier
 
 function deleteProduct() {
@@ -114,104 +119,135 @@ function deleteProduct() {
   for (let i = 0; i < deleteItem.length; i++) {
     const itemClass = deleteItem[i];
     itemClass.onclick = () => {
-      basket = basket.filter(product => product.idOfProduct + product.colorOfProduct != itemClass.id);
+      basket = basket.filter(
+        (product) =>
+          product.idOfProduct + product.colorOfProduct != itemClass.id
+      );
       localStorage.setItem("basket", JSON.stringify(basket));
       location.reload();
     };
   }
 }
 
-
-
 //  GESTION DU FORMULAIRE
-
 
 // Récupérer et analyser les données saisies par l’utilisateur dans le formulaire
 
 // 1/ l'utilisateur va remplir les champs il faut donc mettre les test de valeur a ce niveau avec des REGEX (pas de nombre dans nom et prénom, @ obligatoire dans email)Si problème, message d'erreur dans le champs posant problème
 
 // Stockage des regex
+
 let validFirstName = /^[a-zA-Z\-]+$/g;
 let validLastName = /^[a-zA-Z\-]+$/g;
 let validAdress = /^[a-zA-Z\-\0-9]+$/g;
-let validCity= /^[a-zA-Z\-]+$/g;
-let validEmail= /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+let validCity = /^[a-zA-Z\-]+$/g;
+let validEmail =
+  /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
 
 // recupération et stockage des champs du formulaire
 
 let firstNameInput = document.getElementById("firstName");
-// Ecoute des changements qui sont fait sur les champs. console.log de ce qui a été mis dans le champs par l'utilisateur
-firstNameInput.addEventListener('change', function(){
-  console.log(firstNameInput.value);
-  if(validFirstName.test(firstNameInput.value)){
-    console.log("le test du prénom est validé")
-    document.getElementById("firstNameErrorMsg").innerText="champ validé."
-
-  }else{
-    document.getElementById("firstNameErrorMsg").innerText="Prénom incorrect. Merci de ne pas utiliser de chiffres et caractères spéciaux."
-  }
-});
 let lastNameInput = document.getElementById("lastName");
-// Ecoute des changements qui sont fait sur les champs. console.log de ce qui a été mis dans le champs par l'utilisateur
-lastNameInput.addEventListener('change', function(){
-  console.log(lastNameInput.value);
-  if(validLastName.test(lastNameInput.value)){
-    console.log("le test du nom de famille est validé")
-    document.getElementById("lastNameErrorMsg").innerText="champ validé."
-
-  }else{
-    document.getElementById("lastNameErrorMsg").innerText="Nom incorrect. Merci de ne pas utiliser de chiffres et caractères spéciaux."
-  }
-});
-let adressInput = document.getElementById("address");
-// Ecoute des changements qui sont fait sur les champs. console.log de ce qui a été mis dans le champs par l'utilisateur
-adressInput.addEventListener('change', function(){
-  console.log(adressInput.value);
-  if(validAdress.test(adressInput.value)){
-    console.log("le test de l'adresse est validé")
-    document.getElementById("addressErrorMsg").innerText="champ validé."
-
-  }else{
-    document.getElementById("addressErrorMsg").innerText="Adresse incorrect. Merci de ne pas utiliser de caractères spéciaux."
-  }
-});
+let addressInput = document.getElementById("address");
 let cityInput = document.getElementById("city");
-// Ecoute des changements qui sont fait sur les champs. console.log de ce qui a été mis dans le champs par l'utilisateur
-cityInput.addEventListener('change', function(){
-  if(validCity.test(cityInput.value)){
-    console.log("le test de la ville est validé")
-    document.getElementById("cityErrorMsg").innerText="champ validé."
-  }else{
-    document.getElementById("cityErrorMsg").innerText="Ville incorrect. Merci de ne pas utiliser de caractères spéciaux."
-  }
-});
 let emailInput = document.getElementById("email");
+
+// creation de variables qui vont stocker la valeur de l'input validé
+let firstNameChecked;
+let lastNameChecked;
+let addressChecked;
+let cityChecked;
+let emailChecked;
+
+let contact = {
+  firstName: firstNameChecked,
+  lasName: lastNameChecked,
+  address: addressChecked,
+  city: cityChecked,
+  email: emailChecked,
+};
+
 // Ecoute des changements qui sont fait sur les champs. console.log de ce qui a été mis dans le champs par l'utilisateur
-emailInput.addEventListener('change', function(){
-  if(validEmail.test(emailInput.value)){
-    console.log("le test de l'email est validé")
-    document.getElementById("emailErrorMsg").innerText="champ validé."
-  }else{
-    document.getElementById("emailErrorMsg").innerText="email incorrect. exemple de mail valide : *****@****.fr"
+// Test des inputs renseignés par l'utilisateur versus regex.
+firstNameInput.addEventListener("change", function () {
+  console.log(firstNameInput.value);
+  if (validFirstName.test(firstNameInput.value)) {
+    console.log("le test du prénom est validé");
+    document.getElementById("firstNameErrorMsg").innerText = "champ validé.";
+    firstNameChecked = firstNameInput.value;
+  } else {
+    document.getElementById("firstNameErrorMsg").innerText =
+      "Prénom incorrect. Merci de ne pas utiliser de chiffres et caractères spéciaux.";
+  }
+});
+lastNameInput.addEventListener("change", function () {
+  console.log(lastNameInput.value);
+  if (validLastName.test(lastNameInput.value)) {
+    console.log("le test du nom de famille est validé");
+    document.getElementById("lastNameErrorMsg").innerText = "champ validé.";
+    lastNameChecked = lastNameInput.value;
+  } else {
+    document.getElementById("lastNameErrorMsg").innerText =
+      "Nom incorrect. Merci de ne pas utiliser de chiffres et caractères spéciaux.";
+  }
+});
+addressInput.addEventListener("change", function () {
+  console.log(addressInput.value);
+  if (validAdress.test(addressInput.value)) {
+    console.log("le test de l'adresse est validé");
+    document.getElementById("addressErrorMsg").innerText = "champ validé.";
+    addressChecked = addressInput.value;
+  } else {
+    document.getElementById("addressErrorMsg").innerText =
+      "Adresse incorrect. Merci de ne pas utiliser de caractères spéciaux.";
+  }
+});
+cityInput.addEventListener("change", function () {
+  if (validCity.test(cityInput.value)) {
+    console.log("le test de la ville est validé");
+    document.getElementById("cityErrorMsg").innerText = "champ validé.";
+    cityChecked = cityInput.value;
+  } else {
+    document.getElementById("cityErrorMsg").innerText =
+      "Ville incorrect. Merci de ne pas utiliser de caractères spéciaux.";
+  }
+});
+emailInput.addEventListener("change", function () {
+  if (validEmail.test(emailInput.value)) {
+    console.log("le test de l'email est validé");
+    document.getElementById("emailErrorMsg").innerText = "champ validé.";
+    emailChecked = emailInput.value;
+  } else {
+    document.getElementById("emailErrorMsg").innerText =
+      "email incorrect. exemple de mail valide : *****@****.fr";
   }
 });
 
+// 2/ l'utilisateur va cliquer sur le bouton "commander". Il faut donc écouter ce clique
+// 2.1 récuperer le buton commander et le stocker dans un variable
+let btnOrder = document.getElementById("order");
+// 2.2 écouter la variable qui contient le button avec event 'click'
+btnOrder.addEventListener("click", function () {
+  // if(){
 
+  // }else{
 
+  // }
+  console.log(contact);
+});
 
-// 2/ l'utilisateur va cliquer sur le bouton "commander"
 // 3/ l'event click sur le bouton va envoyer les informations à l'API
-        // 3.1 Creer un objet "contact" qui contiendra toutes les données de la commande (firstName, lastName, adresse, city, email)
-        // 3.2 Creer un tableau "produits" qui contient un id
-        // 3.3 Stringify l'objet commande pour l'avoir au format JSON 
-        // 3.4 Envoyer une requete POST à l'API qui contient le JSON de l'objet "commande".
-        // 3.5 Redirection vers la page confirmation avec message de confirmation contenant l'id de la commande envoyé
+// 3.1 Creer un objet "contact" qui contiendra toutes les données de la commande (firstName, lastName, adresse, city, email)
 
+// 3.2 Creer un tableau "produits" qui contient un id
 
+let produits = [];
+// 3.3 Stringify l'objet commande pour l'avoir au format JSON
+// 3.4 Envoyer une requete POST à l'API qui contient le JSON de l'objet "commande".
+// 3.5 Redirection vers la page confirmation avec message de confirmation contenant l'id de la commande envoyé 1 seul
 
 // Afficher un message d’erreur si besoin (par exemple lorsqu’un utilisateur renseigne “bonjour” dans le champ “e-mail”).
 
-
-
-
 // Constituer un objet contact (à partir des données du formulaire) et un tableau de produits.
+
+// PLAN DE TEST
